5109fd137c467c965fead03f31e8245a074d4720
Renjie Luo <96955451+Jerryluorj@users.noreply.github.com>
2024-06-12 16:29:15 -0400

    Add files via upload

--- CARA_complement.R
+++ CARA_complement.R
@@ -1,5 +1,3 @@
-
-
 #permuted block assignment for first 2m0 patients
 permuted_block = function(n, block_size = 4) {
     assignments = numeric(n)
@@ -25,14 +23,12 @@ permuted_block = function(n, block_size = 4) {
   return(assignments)
 }
 
-
+#generate patients
 Zhao_generate_data = function(n, m0 = 10, k, beta, isResponseBinary) {
   t = c(permuted_block(2 * m0), rep(NA, n - 2 * m0))
-  # Z1=sample(c(1,-1),n,replace = T)
-  # Z2=sample(c(1,-1),n,replace = T)
   X = sample(c(1, -1), n, replace = T)
   Z = replicate(k, sample(c(1, -1), n, replace = T))
-  # py=1/(1+exp(-(0.5+1*X-0.5*X*t+0.5*Z1+0.5*Z2)))
+
   if (isResponseBinary) {
   py = 1 / (1 + exp(-(beta[1]*t+ beta[2]*(1-t) + beta[3] * X + beta[4] * X * t +
                         rowSums(beta[5:length(beta)] * Z))))
@@ -45,7 +41,6 @@ Zhao_generate_data = function(n, m0 = 10, k, beta, isResponseBinary) {
       rowSums(beta[5:length(beta)] * Z)+rnorm(n)
   }
 
-  #return(data.frame(cbind(X,Z1,Z2,Y,t)))
   patients=cbind(X, Z, Y, t)
   colnames(patients)[1]="X"
   colnames(patients)[2:(1+k)]=paste0("Z",1:k)
@@ -54,16 +49,4 @@ Zhao_generate_data = function(n, m0 = 10, k, beta, isResponseBinary) {
   return(patients)
 }
 
-d1 = Zhao_generate_data(n=400, beta=c(0.5,-0.5, 0.5, 0.5,0.5,0.5),k = 3, isResponseBinary = F)
-nrow(d1)
-colnames(d1)
-paste0("Z",1:3)
-colnames(d1)[1]="X"
-colnames(d1)[2:(1+2)]=paste0("Z",1:2)
-colnames(d1)[2+2]="Y"
-colnames(d1)[3+2]="t"
 
-setequal(d1[1,paste0("Z",1:2)],c(-1,-1))
-data_before=d1[1:20,]
-lm_before=lm(data_before[,"Y"]~data_before[,"t"]+(1-data_before[,"t"])+
-               data_before[,"X"]data_before[,"X"]*data_before[,"t"])
